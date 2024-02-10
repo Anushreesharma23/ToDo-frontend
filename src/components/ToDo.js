c1979ca243512dabc4a4d6984c364f8adff633cc
Anushree Sharma <98041426+Anushreesharma23@users.noreply.github.com>
2024-02-10 14:26:24 +0530

    Update ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import {BiEdit} from "react-icons/bi"
 import {AiFillDelete} from "react-icons/ai"
 
-const ToDo = ({text, updateMode, deleteToDo}) => {
+const ToDo = ({text, deleteToDo}) => {
   return (
     <div className="todo">
         <div className="text">{text}</div>
